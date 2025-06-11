8bf0bc329df204c5361a91827d49997c0096b9e2
forword <1tvink122@mail.ru>
2025-06-11 19:00:27 +0300

    фикс для версела7

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,4 +1,8 @@
-import DonateClient from './donate-client'
+import dynamic from 'next/dynamic'
+
+const DonateClient = dynamic(() => import('./donate-client'), {
+  ssr: false,
+})
 
 export default function DonatePage() {
   return <DonateClient />
